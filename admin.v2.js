246409c07fc48600b2afd7b2a447170a4f776c4b
justtme188 <marketingyosua@gmail.com>
2025-09-09 20:35:43 +0700

    Update admin.v2.js

--- admin.v2.js
+++ admin.v2.js
@@ -1,29 +1,19 @@
-async function loadFooter() {
-  try {
-    const res = await fetch("server-info.php");
-    if (!res.ok) throw new Error("Gagal ambil data server-info");
-    const data = await res.json();
-
-    const footerHTML = `
-      <footer class="py-4 bg-dark mt-auto">
-        <div class="container-fluid px-4">
-          <div class="d-flex align-items-center justify-content-between small" style="margin:auto;width:fit-content;">
-            <div class="text-muted text-center">
-              <img alt="JELLPLAY" 
-                   src="/" 
-                   style="max-width:10%;height:auto;">
-              <br>Copyright &copy; 
-             JELLPLAY ${new Date().getFullYear()}
-            </div>
+function loadFooter() {
+  const footerHTML = `
+    <footer class="py-4 bg-dark mt-auto">
+      <div class="container-fluid px-4">
+        <div class="d-flex align-items-center justify-content-between small" style="margin:auto;width:fit-content;">
+          <div class="text-muted text-center">
+            <img alt="JELLPLAY" 
+                 src="https://cdn.jsdelivr.net/gh/justtme188/footer@00f0fecbfd9e2395b9020b5e395e31e5a8460842/logo.png" 
+                 style="max-width:10%;height:auto;">
+            <br>Copyright &copy; 
+            JELLPLAY ${new Date().getFullYear()}
           </div>
         </div>
-      </footer>
-    `;
-
-    document.getElementById("layoutAuthentication_footer").innerHTML = footerHTML;
-  } catch (err) {
-    console.error("Error:", err);
-  }
+      </div>
+    </footer>
+  `;
+  document.getElementById("layoutAuthentication_footer").innerHTML = footerHTML;
 }
-
 document.addEventListener("DOMContentLoaded", loadFooter);
